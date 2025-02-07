cb6aca616ddec492fb308d042f9c30b3026078e9
mouse <kufreedward26@gmail.com>
2025-02-07 03:57:30 +0100

    uses immutability

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -1,8 +1,8 @@
 
 
-public class ChatMessage implements Runnable {
-    public ChatUser recipient;
-    public ChatMessageContent messageContent;
+public final class ChatMessage implements Runnable {
+    private final ChatUser recipient;
+    private final ChatMessageContent messageContent;
 
     /**
      *
